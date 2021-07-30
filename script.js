001dc90744aa57a99beb620b7e9b12bcff42075b
Julieta <julieta.pontino@gmail.com>
2021-07-29 23:39:15 -0300

    correct exercise 4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Ejercicio 1
 
-let num = [1,2,3,4,5,6,7,8,9,10];
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for(let i = 0;i < num.length; i++) {
-    console.log(num[i])
+for (let i = 0; i < num.length; i++) {
+  console.log(num[i]);
 }
 
 // Ejercicio 2
@@ -11,23 +11,23 @@ for(let i = 0;i < num.length; i++) {
 let x = 5;
 
 for (let i = 0; i <= x; i++) {
-   console.log(Math.floor(i))
+  console.log(Math.floor(i));
 }
 
 // Ejercicio 3
 
-let nums = [1,2,3,4,5,6,7,8,9,10];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let i = 1;
 
-while(i <= nums.length) {
-    console.log(i);
-    i= i + 1;
+while (i <= nums.length) {
+  console.log(i);
+  i = i + 1;
 }
 
 // Ejercicio 4
 
-for ( let i = 0; i <= 50; i++) {
-    if ( i % 2== 0) {
-        console.log(i)
-    }
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
 }
