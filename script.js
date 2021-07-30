d5251a6b7f8266f4355d2a55db5771e1a6c8e795
Julieta <julieta.pontino@gmail.com>
2021-07-29 21:01:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//Ejercicio 1
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+for(let i = 0;i < num.length; i++) {
+    console.log(num[i])
+}
+
+// Ejercicio 2
+
+let x = 5;
+
+for (let i = 0; i <= x; i++) {
+   console.log(Math.floor(i))
+}
+
+// Ejercicio 3
+
+let nums = [1,2,3,4,5,6,7,8,9,10];
+let i = 1;
+
+while(i <= nums.length) {
+    console.log(i);
+    i= i + 1;
+}
+
+// Ejercicio 4
+
+for ( let i = 0; i <= 50; i++) {
+    if ( i % 2== 0) {
+        console.log(i)
+    }
+}
